799cf86812ef050418c2adac196ffa2bc1f753b5
Viktoriia Hudz <9095455@stud.op.edu.ua>
2025-10-26 21:15:01 +0200

    Lab5

--- draw/src/main/java/ua/opnu/model/DrawShape.java
+++ draw/src/main/java/ua/opnu/model/DrawShape.java
@@ -10,17 +10,22 @@ public class DrawShape {
 
     public static DrawShape newInstance(int shapeType) {
         DrawShape shape = null;
+
         if (shapeType == DrawShape.SHAPE_RECTANGLE) {
             shape = new Rectangle();
         } else if (shapeType == DrawShape.SHAPE_ROUNDED_RECT) {
             shape = new RoundedRectangle();
+        } else if (shapeType == DrawShape.SHAPE_ELLIPSE) {
+            shape = new Ellipse();
         }
+
         return shape;
     }
 
     // Константи для типів фігур
     public static final int SHAPE_RECTANGLE = 0;
     public static final int SHAPE_ROUNDED_RECT = 1;
+    public static final int SHAPE_ELLIPSE = 2;
 
     // Початкова та кінцева точки
     private Point startPoint;
